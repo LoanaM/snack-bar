347780b5442548d41e9fa134ad0e042a566cd946
LoanaWitch <loanawitch@live.com>
2018-03-23 13:05:37 +0100

    +spazio

--- jsnack2.js
+++ jsnack2.js
@@ -10,10 +10,10 @@ var split1 = parole[0].split("")
 var split2 = parole[1].split("")
 
 if (split1.length < split2.length) {
-  document.write (parole[0])
+  document.write (parole[0] + " ")
   document.write (parole[1])
 }
 else {
-  document.write (parole[1])
+  document.write (parole[1] + " ")
   document.write (parole[0])
 }
