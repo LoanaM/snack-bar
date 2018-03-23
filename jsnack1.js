e8b0de7da45dc00f5b575db3dd1e041bfb122cb1
LoanaWitch <loanawitch@live.com>
2018-03-23 12:47:01 +0100

    snack1

---
+++ jsnack1.js
@@ -0,0 +1,14 @@
+var numeri = []
+
+for (var i = 0; i < 2; i++) {
+  num = parseInt(prompt("Inserisci un numero:"));
+  numeri.push(num)
+}
+console.log (numeri)
+
+if (numeri[0]>numeri[1]) {
+  document.write (numeri[0])
+}
+else {
+  document.write (numeri[1])
+}
