feaa3c6113c328876dd073079a9d6fe621c27e8e
LoanaWitch <loanawitch@live.com>
2018-03-24 13:19:08 +0100

    js6 ok

---
+++ jsnack6.js
@@ -0,0 +1,9 @@
+var invited = ["Gino", "Loana", "Marco", "Rosa", "Romualdo"];
+var name = prompt ("Inserisci il tuo nome:");
+
+if (invited.includes(name)) {
+  alert ("Sei nella lista, puoi etrare!")
+}
+else {
+  alert ("Mi spiace, non puoi entrare!")
+}
