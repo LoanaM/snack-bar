a89939be0b105b6c2e2d0ebe7c341decac74a9f5
LoanaWitch <loanawitch@live.com>
2018-03-24 14:39:51 +0100

    complete %

--- jsnack7.js
+++ jsnack7.js
@@ -30,22 +30,35 @@ var invited8 = {
   "Nome" : "Romualdo",
   "Cognome" : "Rossi",
 }
+var invited9 = {
+  "Nome" : "Pierpaolo",
+  "Cognome" : "Rossi",
+}
+var invited10 = {
+  "Nome" : "Alien",
+  "Cognome" : "Venusian",
+}
 
-
-var maschili = ["Mario", "Marco", "Simone", "Gino", "Luca", "Paolo", "Romualdo"];
+var maschili = ["Mario", "Marco", "Simone", "Gino", "Luca", "Paolo", "Romualdo", "Pierpaolo"];
 var femminili = ["Loana", "Maria", "Martina", "Giulia", "Rosa", "Clara", "Claudia"];
 var maleinvited = 0
 var femaleinvited = 0
-var allinvited = [invited1, invited2, invited3, invited4, invited5, invited6, invited7, invited8];
+var allinvited = [invited1, invited2, invited3, invited4, invited5, invited6, invited7, invited8, invited9, invited10];
 
 for (var i = 0; i < allinvited.length; i++) {
       if (maschili.includes(allinvited[i].Nome)) {
         maleinvited++
       }
-      else {
+      else if (femminili.includes(allinvited[i].Nome)){
         femaleinvited++
       }
 }
-
 console.log ("F " +(femaleinvited))
 console.log ("M " +(maleinvited))
+console.log (percentuale(femaleinvited, allinvited.length))
+console.log (percentuale(maleinvited, allinvited.length))
+
+
+function percentuale(num1, tot) {
+  return num1 / tot * 100
+}
