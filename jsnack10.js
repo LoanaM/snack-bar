553662ffbf6502404ef21d54588512923f273c26
LoanaWitch <loanawitch@live.com>
2018-03-26 01:03:02 +0200

    js10 ok

--- jsnack10.js
+++ jsnack10.js
@@ -1,42 +1,120 @@
+// Crea una serie di oggetti che rappresentano film, contenenti: nome del film, un array di attori, la durata in minuti, un array di voti ricevuti dalla critica e un genere. Scrivere un software che:
+// Calcoli la durata media dei film
+// Stampi i nomi del film che durano più di 180 minuti
+// Stampi il voto medio dei film in cui hanno recitato sia “Brad Pitt” che “Angelina Jolie” (l’output dovrà quindi essere del tipo “Mr & Mrs Smith = 4.3”)
+// Indicare la media dei voti per tutti i film appartenenti ad un genere (l’output dovrà essere del tipo “Azione = 3.8”, “Romantici = 39”...etc)
 
-// Fai inserire un numero, che chiameremo N, all’utente. Genera N array, ognuno formato da 10 numeri casuali da 1 a 100.
-// Al termine della generazione, stampa l’array la cui somma degli elementi è la maggiore.
-
-var n = parseInt(prompt("Inserisci un numero:"));
-
-arrayGroup = [];
-somma = 0;
-somme = [];
-
-//creo nuovi n array
-for (var i = 0; i < n; i++) {
-  arrayGroup.push("array" + (i));
-  arrayGroup[i] = [];
+var film1 = {
+  "nome" : "Mr & Mrs Smith",
+  "attori" : ["Brad Pitt", "Angelina Jolie", "Raoul Bova"],
+  "durata" : 120,
+  "voti" : [7, 7, 5, 6, 8],
+  "genere" : "azione",
+}
+var film2 = {
+  "nome" : "Ben Hur",
+  "attori" : ["Charlton Heston", "Stephen Boyd"],
+  "durata" : 200,
+  "voti" : [10, 9, 10, 8, 8],
+  "genere" : "storico",
+}
+var film3 = {
+  "nome" : "Troy",
+  "attori" : ["Brad Pitt", "Orlando Bloom", "Eric Bana"],
+  "durata" : 190,
+  "voti" : [7, 7, 9, 6, 8],
+  "genere" : "storico",
+}
+var film4 = {
+  "nome" : "A Mighty Heart",
+  "attori" : ["Brad Pitt", "Angelina Jolie"],
+  "durata" : 108,
+  "voti" : [8, 7, 6, 6, 8],
+  "genere" : "drammatico",
+}
+var film5 = {
+  "nome" : "Tomb Raider",
+  "attori" : ["Angelina Jolie"],
+  "durata" : 120,
+  "voti" : [7, 5, 4, 6, 7],
+  "genere" : "azione",
+}
+var film6 = {
+  "nome" : "By the sea",
+  "attori" : ["Brad Pitt", "Angelina Jolie"],
+  "durata" : 132,
+  "voti" : [7, 9, 8, 6, 8],
+  "genere" : "drammatico",
+}
+var film7 = {
+  "nome" : "I pirati dei Caraibi",
+  "attori" : ["Johnny Depp", "Penelope Cruz"],
+  "durata" : 150,
+  "voti" : [9, 7, 8, 6, 8],
+  "genere" : "azione",
 }
 
-for (var z = 0; z < arrayGroup.length; z++) {
-  for (var u = 0; u < 10; u++) {
-    number = Math.floor((Math.random() * 100) + 1);
-    arrayGroup[z].push(number)
+var films = [film1, film2, film3,film4,film5,film6,film7]
+
+//calcolo la durata media dei film
+  var somma = 0;
+  for ( var i=0; i<films.length; i++ ) {
+    somma = films[i].durata + somma
+  }
+  result = somma / films.length
+
+console.log(result)
+
+//stampo i film>180
+for (var i = 0; i < films.length; i++) {
+  if (films[i].durata > 180) {
+    document.write(films[i].nome + " ")
   }
 }
-console.log (arrayGroup)
 
-//faccio le somme di tutti gli array e li inserisco in "somme"
-for (var a = 0; a < arrayGroup.length; a++) {
-  for (var i = 0; i < 10; i++) {
-  somma+= arrayGroup[a][i]
+//stampo la media dei film con Brangelina
+for (var i = 0; i < films.length; i++) {
+  if ((films[i].attori.includes("Angelina Jolie")) && (films[i].attori.includes("Brad Pitt"))) {
+    console.log (films[i].nome + ": " + media(films[i].voti))
   }
-  somme.push(somma)
 }
-console.log (somme)
 
-//calcolo la somma più grande
-var massimo = arrayGroup[0];
+//media per genere
+var mediagenre = 0;
+var dividendi = 0;
+for (var i = 0; i < films.length; i++) {
+    if (films[i].genere=="azione") {
+      mediagenre+= media(films[i].voti)
+      dividendi++
+    }
+}
+var mediaction = (mediagenre / dividendi)
+console.log ("Azione = " + mediaction)
 
-// for(var i=0; i<somme.length; i++) {
-//       if(somme[i]>maggiore ) {
-//         maggiore=somme[i];
-//       }
-// }
-console.log (massimo)
+for (var i = 0; i < films.length; i++) {
+    if (films[i].genere=="storico") {
+      mediagenre+= media(films[i].voti)
+      dividendi++
+    }
+}
+var mediastory = (mediagenre / dividendi)
+console.log ("Storico = " + mediastory)
+
+for (var i = 0; i < films.length; i++) {
+    if (films[i].genere=="drammatico") {
+      mediagenre+= media(films[i].voti)
+      dividendi++
+    }
+}
+var mediadrama = (mediagenre / dividendi)
+console.log ("Drammatico = " + mediadrama)
+
+
+
+function media (inputArray) {
+  var somma = 0;
+  for ( var i=0; i<inputArray.length; i++ ) {
+    somma = inputArray[i] + somma
+  }
+  return somma / inputArray.length
+}
