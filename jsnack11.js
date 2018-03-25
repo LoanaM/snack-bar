cff153b714d9594d4b5a8a76db1e47d7ac84194a
LoanaWitch <loanawitch@live.com>
2018-03-26 01:56:29 +0200

    js11 + 12 ook

--- jsnack11.js
+++ jsnack11.js
@@ -1,11 +1,23 @@
-var oggi=new Date()
-var comple=new Date(2018,11,23)
+//countdown al mio comple
+
+var oggi = new Date()
+var comple = new Date(2018,11,23)
 
 //conteggia i sec dal 1/1/1970
 function countgg(tempo) {
-return(Math.floor(tempo.getTime()/(1000))) //(1000*60*60*24)=conta i gg
+  return(Math.floor(tempo.getTime()/(1000))) //(1000*60*60*24)=conta i gg
 }
 
 secmancanti = (countgg(comple) - countgg(oggi))
 
-console.log(secmancanti)
+
+var countdown = window.setInterval (timer, 1000);
+
+function timer () {
+  console.log (secmancanti)
+  secmancanti--
+  if (secmancanti<0) {
+    window.clearTimeout (countdown);
+    console.log("AUGURI!!")
+  }
+}
